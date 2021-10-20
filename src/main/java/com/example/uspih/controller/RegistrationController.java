a1e7e1353f545996c0ee609b432d4db0805eb208
vadimkain <kain.vadim50@gmail.com>
2021-10-20 14:14:19 +0300

    Удалили лишнее, оставили регистрацию и вход

--- src/main/java/com/example/uspih/controller/RegistrationController.java
+++ src/main/java/com/example/uspih/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import com.example.uspih.domain.dto.CaptchaResponseDto;
 import com.example.uspih.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
